53b3593634d3255cef048d17e297f0ae3605fd5c
NZerr <nathan.zerr@gmail.com>
2021-07-12 14:42:28 -0400

    deploying

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true
 });
+
+// mongoose.connect('mongodb://localhost/workout', {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true,
+// });
 
 const workoutSeed = [
   {
